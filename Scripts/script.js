f53cfa88853eb505a138bf4e1006f49d9a6a0aa4
Bram Giesbers <rv669912@edu.rocrivor.nl>
2025-05-23 09:10:39 +0200

    UPDATE

---
+++ Scripts/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('keydown', function(e) {
+    if (e.code === 'Space') {
+      const img = document.createElement('img');
+      img.src = 'Src/Beer.jpg'; // Zorg dat het pad klopt
+
+      // Styling in JS
+      Object.assign(img.style, {
+        position: 'absolute',
+        left: Math.random() * window.innerWidth + 'px',
+        top: Math.random() * window.innerHeight + 'px',
+        width: '100px',
+        height: 'auto',
+        zIndex: '9999', // bovenop alles
+        pointerEvents: 'none' // zodat je achterliggende elementen kunt blijven aanklikken
+      });
+
+      document.body.appendChild(img);
+    }
+  });
