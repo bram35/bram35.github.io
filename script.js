27ea905f4dd38cebaa87a3ddc5345c237e28903c
bram35 <rv669912@edu.rocrivor.nl>
2025-11-03 10:14:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
-document.addEventListener('keydown', function(e) {
-    if (e.code === 'Space') {
-      const img = document.createElement('img');
-      img.src = 'Src/Beer.jpg'; // Zorg dat het pad klopt
-
-      // Styling in JS
-      Object.assign(img.style, {
-        position: 'absolute',
-        left: Math.random() * window.innerWidth + 'px',
-        top: Math.random() * window.innerHeight + 'px',
-        width: '100px',
-        height: 'auto',
-        zIndex: '999', // bovenop alles
-        pointerEvents: 'none' // zodat je achterliggende elementen kunt blijven aanklikken
-      });
-
-      document.body.appendChild(img);
+class Dier {
+    constructor(naam, soort, leeftijd) {
+        this.naam = naam;
+        this.soort = soort;
+        this.leeftijd = leeftijd;
     }
-  });
+
+    maak_geluid() {
+        console.log(`${this.naam} maakt een geluid.`);
+    }
+}
+//paard
+class Paard extends Dier {
+    constructor(naam, leeftijd, kleur) {
+        super(naam, 'paard', leeftijd);
+        this.kleur = kleur;
+    }
+
+    paardgeluid() {
+        console.log(`${this.naam} zegt heuuughhh`);
+    }
+}
+
+const rigiel = new Paard('rigiel', 2, 'wit');
+console.log(rigiel);
+rigiel.maak_geluid();
+rigiel.paardgeluid();
+//hagedis
+class Hagedis extends Dier {
+    constructor(naam, leeftijd, kleur) {
+        super(naam, 'hagedis', leeftijd);
+        this.kleur = kleur;
+    }
+
+    hagedisgeluid() {
+        console.log(`${this.naam} zegt ssssssssss`);
+    }
+}
+
+const joop = new Hagedis('joop', 1, 'groen');
+console.log(joop);
+joop.maak_geluid();
+joop.hagedisgeluid();
+//hond
+class hond extends Dier {
+    constructor(naam, leeftijd, kleur) {
+        super(naam, 'hond', leeftijd);
+        this.kleur = kleur;
+    }
+
+    hondgeluid() {
+        console.log(`${this.naam} zegt woef`);
+    }
+}
+
+const jemairo = new hond('jemairo', 1, 'bruin');
+console.log(jemairo);
+jemairo.maak_geluid();
+jemairo.hondgeluid();
